56378ea3d6c1e6bcadc75c7b45cd851fc8197a46
Doychev,Viktor V <v.doychev@student.fontys.nl>
2023-01-04 18:23:45 +0200

    Statistics implemented

--- individualproject_frontend/src/apis/StatisticsAPI.js
+++ individualproject_frontend/src/apis/StatisticsAPI.js
@@ -2,14 +2,15 @@ import axios from "axios";
 const URL = "http://localhost:8080/statistics";
 
 const GetStatistics = {
-  loadTruckStatistics: () =>
-    axios.get(URL + "/trucks").then((response) => response.data),
-  loadOrderStatisticsByApproval: (approved) =>
-    axios.get(URL + `/orders/${approved}`).then((response) => response.data),
-  loadOrderStatisticsByDate: (date) =>
-    axios.get(URL + `/ordersDate/${date}`).then((response) => response.data),
-  loadUserStatisticsByRole: (role) =>
-    axios.get(URL + `/users/${role}`).then((response) => response.data),
+  loadStatistics: () => axios.get(URL).then((response) => response.data),
+  // loadTruckStatistics: () =>
+  //   axios.get(URL + "/trucks").then((response) => response.data),
+  // loadOrderStatisticsByApproval: (approved) =>
+  //   axios.get(URL + `/orders/${approved}`).then((response) => response.data),
+  // loadOrderStatisticsByDate: (date) =>
+  //   axios.get(URL + `/ordersDate/${date}`).then((response) => response.data),
+  // loadUserStatisticsByRole: (role) =>
+  //   axios.get(URL + `/users/${role}`).then((response) => response.data),
 };
 
 export default GetStatistics;
--- individualproject_frontend/src/pages/StatisticsPage.js
+++ individualproject_frontend/src/pages/StatisticsPage.js
@@ -2,11 +2,15 @@ import StaticsAPI from "../apis/StatisticsAPI.js";
 import React, { useState, useEffect } from "react";
 
 const initialState = {
-  stats: [],
-  role: "",
+  // stats: [],
+  trucks: "",
   approved: "",
   notApproved: "",
-  date: "",
+  ordersToday: "",
+  ordersBefore: "",
+  ordersAfter: "",
+  employees: "",
+  customers: "",
   isFetching: false,
   hasError: false,
 };
@@ -23,7 +27,15 @@ const reducer = (state, action) => {
       return {
         ...state,
         isFetching: false,
-        stats: action.payload,
+        // stats: action.payload,
+        approved: action.payload.approved,
+        notApproved: action.payload.notApproved,
+        trucks: action.payload.trucks,
+        ordersToday: action.payload.ordersToday,
+        ordersBefore: action.payload.ordersBefore,
+        ordersAfter: action.payload.ordersAfter,
+        employees: action.payload.employees,
+        customers: action.payload.customers,
       };
     case "FETCH_ADV_FAILURE":
       return {
@@ -40,12 +52,14 @@ const StaticsPage = () => {
   React.useEffect(() => {
     //console.log("Starting dispatching!");
     dispatch({ type: "FETCH_ADV-REQUEST" });
-    StaticsAPI.loadOrderStatisticsByApproval("approved")
+    console.log(StaticsAPI.loadStatistics());
+    StaticsAPI.loadStatistics()
       .then((response) => {
         dispatch({
           type: "FETCH_ADV_SUCCESS",
           payload: response,
         });
+        console.log(response);
       })
       .catch((error) => {
         console.log(error);
@@ -53,7 +67,7 @@ const StaticsPage = () => {
           type: "FETCH_ADV_FAILURE",
         });
       });
-  });
+  }, []);
   //   var myData = {
   //     chart: {
   //       caption: "Orders ratio",
@@ -103,18 +117,147 @@ const StaticsPage = () => {
   //     dataSource: myData,
   //   };
   return (
-    <div className="container">
-      {/* if(state.stats.length > ?0){
-
-        } */}
-      {state.stats.length > 0 ? (
-        <label defaultValue={state.stats}></label>
-      ) : (
-        <span className="noTrucks">
-          Sorry! We don't have any trucks for now...
-        </span>
-      )}
-    </div>
+    <React.Fragment>
+      <div className="main-content">
+        <div className="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+          <div className="container-fluid">
+            <h2 className="mb-5 text-white">Statistics summary</h2>
+            <div className="header-body">
+              <div className="row">
+                <div className="col-xl-3 col-lg-6">
+                  <div className="card card-stats mb-4 mb-xl-0">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col">
+                          <h5 className="card-title text-uppercase text-muted mb-0">
+                            Approved orders
+                          </h5>
+                          <label className="h2 font-weight-bold mb-0">
+                            {state.approved}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6">
+                  <div className="card card-stats mb-4 mb-xl-0">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col">
+                          <h5 className="card-title text-uppercase text-muted mb-0">
+                            Not approved orders
+                          </h5>
+                          <label className="h2 font-weight-bold mb-0">
+                            {state.notApproved}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6">
+                  <div className="card card-stats mb-4 mb-xl-0">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col">
+                          <h5 className="card-title text-uppercase text-muted mb-0">
+                            Customers
+                          </h5>
+                          <label className="h2 font-weight-bold mb-0">
+                            {state.customers}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6">
+                  <div className="card card-stats mb-4 mb-xl-0">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col">
+                          <h5 className="card-title text-uppercase text-muted mb-0">
+                            Employees
+                          </h5>
+                          <label className="h2 font-weight-bold mb-0">
+                            {state.employees}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6">
+                  <div className="card card-stats mb-4 mb-xl-0">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col">
+                          <h5 className="card-title text-uppercase text-muted mb-0">
+                            Trucks
+                          </h5>
+                          <label className="h2 font-weight-bold mb-0">
+                            {state.trucks}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6">
+                  <div className="card card-stats mb-4 mb-xl-0">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col">
+                          <h5 className="card-title text-uppercase text-muted mb-0">
+                            Orders today
+                          </h5>
+                          <label className="h2 font-weight-bold mb-0">
+                            {state.ordersToday}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6">
+                  <div className="card card-stats mb-4 mb-xl-0">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col">
+                          <h5 className="card-title text-uppercase text-muted mb-0">
+                            Orders before today
+                          </h5>
+                          <label className="h2 font-weight-bold mb-0">
+                            {state.ordersBefore}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6">
+                  <div className="card card-stats mb-4 mb-xl-0">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col">
+                          <h5 className="card-title text-uppercase text-muted mb-0">
+                            Orders after today
+                          </h5>
+                          <label className="h2 font-weight-bold mb-0">
+                            {state.ordersAfter}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 export default StaticsPage;
