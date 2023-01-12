6bde93b90c56c39f220f07a397f9ad6f0523c27d
Doychev,Viktor V <v.doychev@student.fontys.nl>
2023-01-12 17:21:35 +0200

    Design changes

--- individualproject_frontend/src/apis/StatisticsAPI.js
+++ individualproject_frontend/src/apis/StatisticsAPI.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+import axiosInterceptor from "../apis/axiosInterceptor.js";
+
 const URL = "http://localhost:8080/statistics";
 
+axiosInterceptor();
 const GetStatistics = {
   loadStatistics: () => axios.get(URL).then((response) => response.data),
   // loadTruckStatistics: () =>
--- individualproject_frontend/src/pages/Register.js
+++ individualproject_frontend/src/pages/Register.js
@@ -73,77 +73,79 @@ function Register() {
   };
   return (
     <div className="container rounded bg-white mt-5 mb-5">
-      <div className="row">
-        <div className="col-md-3 border-right">
-          <div className="d-flex flex-column align-items-center text-center p-3 py-5">
-            <img
-              className="rounded-circle mt-5"
-              width="150px"
-              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-            ></img>
+      <div className="register">
+        <div className="row">
+          <div className="col-md-3 border-right">
+            <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+              <img
+                className="rounded-circle mt-5"
+                width="150px"
+                src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+              ></img>
+            </div>
           </div>
-        </div>
-        <div className="col-md-5 border-right">
-          <div className="p-3 py-5">
-            <form onSubmit={handleFormSubmit}>
-              <div className="d-flex justify-content-between align-items-center mb-3">
-                <h4 className="text-right">Register</h4>
-              </div>
-              {data.errorMessage && (
-                <span className="form-error">{data.errorMessage}</span>
-              )}
-              <div className="row mt-3">
-                <div className="col-md-12">
-                  <label className="labels">First name</label>
-                  <input
-                    className="form-control"
-                    placeholder="First name"
-                    defaultValue={data.firstName}
-                    onChange={handleInputChange}
-                    name="firstName"
-                  ></input>
+          <div className="col-md-5 border-right">
+            <div className="p-3 py-5">
+              <form onSubmit={handleFormSubmit}>
+                <div className="d-flex justify-content-between align-items-center mb-3">
+                  <h4 className="text-right">Register</h4>
                 </div>
-                <div className="col-md-12">
-                  <label className="labels">Last name</label>
-                  <input
-                    className="form-control"
-                    placeholder="Last name"
-                    defaultValue={data.lastName}
-                    onChange={handleInputChange}
-                    name="lastName"
-                  ></input>
+                {data.errorMessage && (
+                  <span className="form-error">{data.errorMessage}</span>
+                )}
+                <div className="row mt-3">
+                  <div className="col-md-12">
+                    <label className="labels">First name</label>
+                    <input
+                      className="form-control"
+                      placeholder="First name"
+                      defaultValue={data.firstName}
+                      onChange={handleInputChange}
+                      name="firstName"
+                    ></input>
+                  </div>
+                  <div className="col-md-12">
+                    <label className="labels">Last name</label>
+                    <input
+                      className="form-control"
+                      placeholder="Last name"
+                      defaultValue={data.lastName}
+                      onChange={handleInputChange}
+                      name="lastName"
+                    ></input>
+                  </div>
+                  <div className="col-md-12">
+                    <label className="labels">Username</label>
+                    <input
+                      className="form-control"
+                      placeholder="Username"
+                      defaultValue={data.username}
+                      onChange={handleInputChange}
+                      name="username"
+                    ></input>
+                  </div>
+                  <div className="col-md-12">
+                    <label className="labels">Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Password"
+                      defaultValue={data.password}
+                      onChange={handleInputChange}
+                      name="password"
+                    ></input>
+                  </div>
                 </div>
-                <div className="col-md-12">
-                  <label className="labels">Username</label>
-                  <input
-                    className="form-control"
-                    placeholder="Username"
-                    defaultValue={data.username}
-                    onChange={handleInputChange}
-                    name="username"
-                  ></input>
+                <div className="mt-5 text-center">
+                  <button
+                    className="btn btn-primary"
+                    disabled={data.isSubmitting}
+                  >
+                    {data.isSubmitting ? "Loading..." : "Create"}
+                  </button>
                 </div>
-                <div className="col-md-12">
-                  <label className="labels">Password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Password"
-                    defaultValue={data.password}
-                    onChange={handleInputChange}
-                    name="password"
-                  ></input>
-                </div>
-              </div>
-              <div className="mt-5 text-center">
-                <button
-                  className="btn btn-primary"
-                  disabled={data.isSubmitting}
-                >
-                  {data.isSubmitting ? "Loading..." : "Create"}
-                </button>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
